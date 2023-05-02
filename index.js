33d0198757b13fe20dc8e7b1149681feeb1ca87c
dev-talukdar <khejurbd@gmail.com>
2023-05-02 15:09:48 +0600

    data loading

--- index.js
+++ index.js
@@ -3,19 +3,43 @@ const app = express()
 const cors = require('cors')
 const port = process.env.PORT || 5000;
 
-const categories = require('./data/categories.json')
+const categories = require('./data/categories.json');
+const news = require('./data/news.json');
 
 app.use(cors());
-
-app.get('/categories', (req, res) =>{
-    res.send(categories)
-})
 
 app.get('/', (req, res) =>{
     res.send('Dragon is Running')
 });
 
+app.get('/categories', (req, res) =>{
+    res.send(categories)
+});
 
+app.get('/news', (req, res) =>{
+    res.send(news)
+}) ;
+
+app.get('/news/:id', (req, res) =>{
+    const id = req.params.id;
+    console.log(id)
+    const selectedNews = news.find(n => n._id ===id);
+    res.send(selectedNews)
+});
+
+app.get('/categories/:id',(req, res) =>{
+const id =  parseInt(req.params.id);
+// const id =   req.params.id;
+
+if (id == 0) {
+    res.send(news)
+}
+else{
+    const categoriesNews = news.filter(n => parseInt(n.category_id) === id)
+    // const categoriesNews = news.filter(n =>  n.category_id === id)
+    res.send(categoriesNews)
+}
+});
 
 app.listen(port, () => {
     console.log(`dragon api is running on port :${port}`)
